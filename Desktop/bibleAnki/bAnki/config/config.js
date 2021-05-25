776180be3c1b52b21a0f93df87764ef907ed2f24
Josiah <josiah.james.fowler@gmail.com>
2021-05-25 11:14:02 -0400

    apiUpdated

---
+++ Desktop/bibleAnki/bAnki/config/config.js
@@ -0,0 +1,3 @@
+const config = {
+    MY_API_TOKEN: '2e5d71a91294792ed187ecf7c3645b31',
+}
---
+++ Desktop/bibleAnki/bAnki/public/js/main.js
@@ -0,0 +1,21 @@
+// require("dotenv").config({ path: "./config/.env" });
+
+document.querySelector('#fetchBibles').addEventListener('click', getFetch)
+
+async function getFetch() {
+    // const todoText = this.parentNode.childNodes[1].innerText
+    console.log('clicked')
+    try {
+        const response = await fetch('https://api.scripture.api.bible/v1/bibles', {
+            method: 'get',
+            headers: { 'accept': 'application/json', 'api-key': '2e5d71a91294792ed187ecf7c3645b31' },
+
+        })
+        const data = await response.json()
+        console.log('json receives')
+        console.log(data)
+
+    } catch (err) {
+        console.log(err)
+    }
+}
