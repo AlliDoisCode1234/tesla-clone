98e80a98a0e243273e0b091c0d60ae4cf2997026
Josiah <josiah.james.fowler@gmail.com>
2021-06-08 16:07:12 -0400

    clean deployment repo

--- src/components/Section.js
+++ src/components/Section.js
@@ -26,7 +26,7 @@ export default Section
 
 
 const Container = styled.div`
-    background-color: blue;
+    
     width: 100%;
     height: 100vh;
     background-image: ${props => `url('/images/${props.bgImage}')`};
